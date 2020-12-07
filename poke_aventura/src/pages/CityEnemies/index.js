a33c229bae601dda21b077ab426bae6b125a0c68
Caio <caiooliv97@gmail.com>
2020-12-07 04:25:23 -0300

    Criada ordenação dos inimigos

--- poke_aventura/src/pages/CityEnemies/index.js
+++ poke_aventura/src/pages/CityEnemies/index.js
@@ -5,34 +5,51 @@ import rocket1 from "../../assets/rocket1.png";
 import rocket2 from "../../assets/rocket2.png";
 import rocket3 from "../../assets/rocket3.png";
 import api from "../../services/api";
+import { mergeSort } from "../../utils/merge";
 
 //const giovanni = "../../assets/giovanni.png";
 
 function CityEnemies() {
   const [enemyTeam, setEnemyTeam] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   async function handleOrganize() {
-    let response = await api.get("/pokemon/1");
-    console.log(response.data);
+    var tempArray = enemyTeam;
+
+    for await (let enemy of enemyTeam) {
+      var teamTotalStrenght = 0;
+      for await (let enemyPokemons of enemy.pokemons) {
+        let response = await api.get(`pokemon/${enemyPokemons + 1}`);
+        var totalForce =
+          response.data.stats[1].base_stat + response.data.stats[2].base_stat;
+        teamTotalStrenght = teamTotalStrenght + totalForce;
+      }
+      tempArray[enemy.index].totalForce = teamTotalStrenght;
+    }
+
+    let teste = mergeSort(tempArray);
+    console.log(teste);
+    setEnemyTeam(teste);
   }
 
   useEffect(() => {
     for (let i = 1; i < 4; i++) {
-      let index;
+      let sprite;
       switch (i) {
         case 1:
-          index = rocket1;
+          sprite = rocket1;
           break;
         case 2:
-          index = rocket2;
+          sprite = rocket2;
           break;
         case 3:
-          index = rocket3;
+          sprite = rocket3;
           break;
       }
 
       let enemy = {
-        index: index,
+        index: i - 1,
+        sprite: sprite,
         pokemons: [],
       };
       for (let j = 0; j < 3; j++) {
@@ -52,7 +69,7 @@ function CityEnemies() {
       {enemyTeam.map((enemy) => {
         return (
           <Container>
-            <img src={enemy.index} />
+            <img src={enemy.sprite} />
             <ButtonPokemon pokemonIndex={enemy.pokemons[0]} />
             <ButtonPokemon pokemonIndex={enemy.pokemons[1]} />
             <ButtonPokemon pokemonIndex={enemy.pokemons[2]} />
